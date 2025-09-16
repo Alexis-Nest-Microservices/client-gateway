8d670aa47a199493aa547a88c8dfd337f1ef4f98
alexis.rueda <alexisrueda34@gmail.com>
2025-09-16 01:27:41 -0600

    Agregar configuración de microservicio de productos y variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-
+  
   await app.listen(envs.port);
 
   logger.log(`Gateway running on port ${envs.port}`);
