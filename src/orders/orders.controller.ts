91f663c13cd53577f3723c98378eb05f81e2ac55
alexis.rueda <alexisrueda34@gmail.com>
2025-09-18 00:57:11 -0600

    Agregar módulo y controlador de órdenes, incluyendo DTOs y configuración de microservicio

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Post, Body, Param, Inject, Query } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { ORDER_SERVICE } from 'src/config';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaginationDto } from 'src/common';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(
+      @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy
+    ) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('createOrder', createOrderDto);
+  }
+
+  @Get()
+  findAll( @Query() paginationDto: PaginationDto) {
+    return this.ordersClient.send('findAllOrders', paginationDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ordersClient.send('findOneOrder', { id });
+  }
+  
+}
